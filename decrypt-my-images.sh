4bb7ca6a6dd2f29d219dde9e3c3ac50322790902
Alex <dropbyalex@gmail.com>
2014-10-15 20:18:28 -0400

    Temporary fix. To encrypt all of your images in .JPG format from the current folder and place into enc folder run encrypt-my-images.sh. To decrypt all images in .JPG format from the current folder and place into dec folder run decrypt-my-images.sh.

--- decrypt-my-images.sh
+++ decrypt-my-images.sh
@@ -6,22 +6,22 @@ echo '(2) We will be decrypting individual files from the /enc/ directory.'
 echo '(3) Decrypted files will be stored in a new (./dec/) directory'
 echo '(4) Using aes-256-cbc'
 
-echo 'Please enter the directory to encrypt: '
-read directory
+# echo 'Please enter the directory to encrypt: '
+# read directory
 
 echo 'Please enter the cypher to use: '
 read cypher
 
 echo 'Making dec directory'
-mkdir "$directory/dec"
+mkdir dec
 
 echo 'Please enter the password to use: '
 read password
 
 echo 'Beginning decryption...'
-for file in "$directory/enc/*.JPG"
+for file in *.JPG
 	do 
-		openssl enc -d $cyper -a -in "$file" -out "$directory/dec/$file" -pass pass:$password
+		openssl enc -d $cyper -a -in "$file" -out "./dec/$file" -pass pass:$password
 done
 echo 'Finished.'
 
--- encrypt-my-images.sh
+++ encrypt-my-images.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 echo 'Hello,'
-echo '(1) We will be using openssl to encrypt your jpg files.'
+echo '(1) We will be using openssl to encrypt your files.'
 echo '(2) We will be encrypting individual files in a directory.'
 echo '(3) Encrypted files will be stored in the subdirectory (./enc/)'
 echo '(4) Using aes-256-cbc'
 
-echo 'Please enter the directory to encrypt: '
-read directory
+# echo 'Please enter the directory to encrypt: '
+# read directory
 
 echo 'Please enter the cypher to use: '
 read cypher
 
 echo 'Making enc directory'
-mkdir "$directory/enc"
+mkdir enc
 
 echo 'Please enter the password to use: '
 read password
 
 echo 'Beginning encryption...'
-for file in "$directory/*.JPG"
+for file in *.JPG
 	do 
-		openssl enc $cyper -a -salt -in "$file" -out "$directory/enc/$file" -pass pass:$password
+		openssl enc $cyper -a -salt -in "$file" -out "./enc/$file" -pass pass:$password
 done
-echo 'Finished.'
 
+echo 'Finished.'
