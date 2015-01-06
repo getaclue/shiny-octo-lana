47c2c70f47e923721b387a3fe97b0c45eae7824e
Alex <dropbyalex@gmail.com>
2015-01-06 14:01:27 -0500

    Second implementation of the answer for learnyounode

--- NodeJS_test/learnyounode/time-server.js
+++ NodeJS_test/learnyounode/time-server.js
@@ -1,12 +1,38 @@
 var port = process.argv[2]
 var net = require('net')
 
-var server = net.createServer(function(socket){
-	var date = new Date()
-	var output = date.getFullYear() + '-0' + (date.getMonth() + 1) + '-0' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + '\n'
+// Second Solution based on the answer to the exercise
+// ////////////////////////////////////////////////////////////////////////////
 
-	socket.write(output)
+function zeroed(i){
+	return (i < 10 ? '0' : '') + i
+}
+
+function dateAndTime(){
+	var date = new Date()
+	return date.getFullYear() + '-' + 
+	zeroed(date.getMonth() + 1) + '-' + 
+	zeroed(date.getDate()) + ' ' + 
+	zeroed(date.getHours()) + ':' + 
+	zeroed(date.getMinutes()) + '\n'
+}
+
+var server = net.createServer(function(socket){
+	console.log('New client connected!')
+	socket.write(dateAndTime())
 	socket.end()
 })
 
 server.listen(Number(port))
+
+// Initial solution (crude)
+// ////////////////////////////////////////////////////////////////////////////
+// var server = net.createServer(function(socket){
+// 	var date = new Date()
+// 	var output = date.getFullYear() + '-0' + (date.getMonth() + 1) + '-0' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + '\n'
+
+// 	socket.write(output)
+// 	socket.end()
+// })
+
+// server.listen(Number(port))
